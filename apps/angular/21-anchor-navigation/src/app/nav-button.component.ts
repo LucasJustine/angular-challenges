f6c977df00320736d4c53cea5a18983d4bd2161e
Lucas Justine <justinelucas91800@gmail.com>
2026-09-23 13:39:59 +0200

    feat(challenge #21): scroll and change page animations

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="href()">
       <ng-content />
     </a>
   `,
@@ -12,6 +13,7 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
   href = input('');
